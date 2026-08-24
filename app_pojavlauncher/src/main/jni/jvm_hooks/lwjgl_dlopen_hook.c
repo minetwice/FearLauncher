f0c26cf2cdbaebc05bf9cd0f012cb9e705a8551a
Twicefear <ytd82774@gmail.com>
2026-08-24 19:29:45 +0530

    fix: Remove GL_NV_shader_noperspective_interpolation from GL extension hooks
    The root cause of Complementary Reimagined crash was found:
    lwjgl_dlopen_hook.c was advertising GL_NV_shader_noperspective_interpolation
    as an available extension via glGetString_hook and glGetStringi_hook.
    Iris saw the extension as available and emitted 'noperspective' keyword
    in shaders, but LTW's GLSL compiler doesn't support it (treats it as
    reserved keyword, causing L0003 error).
    
    Fix: Removed GL_NV_shader_noperspective_interpolation from both
    glGetString_hook extension string and glGetStringi_hook extension array.
    Now Iris will see the extension as NOT available and will NOT emit
    'noperspective' in shaders.
    
    Also adds GL_ARB_shader_texture_lod to the extension list to fix
    Bliss and RedHat warnings.

--- app_pojavlauncher/src/main/jni/jvm_hooks/lwjgl_dlopen_hook.c
+++ app_pojavlauncher/src/main/jni/jvm_hooks/lwjgl_dlopen_hook.c
@@ -48,7 +48,7 @@ static const unsigned char* glGetString_hook(unsigned int name) {
     } else if (name == GL_VENDOR) {
         return (const unsigned char*)"NVIDIA Corporation";
     } else if (name == GL_EXTENSIONS) {
-        return (const unsigned char*)"GL_ARB_direct_state_access GL_ARB_buffer_storage GL_ARB_shader_image_load_store GL_NV_conditional_render GL_EXT_gpu_shader4 GL_EXT_texture_buffer GL_EXT_texture_cube_map_array GL_OES_EGL_image_external_essl3 GL_NV_shader_noperspective_interpolation GL_ARB_shader_objects GL_ARB_vertex_shader GL_ARB_fragment_shader GL_EXT_blend_equation_separate GL_EXT_geometry_shader4 GL_EXT_gpu_program_parameters GL_ARB_instanced_arrays GL_ARB_draw_instanced";
+        return (const unsigned char*)"GL_ARB_direct_state_access GL_ARB_buffer_storage GL_ARB_shader_image_load_store GL_NV_conditional_render GL_EXT_gpu_shader4 GL_EXT_texture_buffer GL_EXT_texture_cube_map_array GL_OES_EGL_image_external_essl3 GL_ARB_shader_texture_lod GL_ARB_shader_objects GL_ARB_vertex_shader GL_ARB_fragment_shader GL_EXT_blend_equation_separate GL_EXT_geometry_shader4 GL_EXT_gpu_program_parameters GL_ARB_instanced_arrays GL_ARB_draw_instanced";
     }
 
     typedef const unsigned char* (*glGetString_pfn)(unsigned int);
@@ -76,7 +76,7 @@ static const unsigned char* glGetStringi_hook(unsigned int name, unsigned int in
             "GL_EXT_texture_buffer",
             "GL_EXT_texture_cube_map_array",
             "GL_OES_EGL_image_external_essl3",
-            "GL_NV_shader_noperspective_interpolation",
+            "GL_ARB_shader_texture_lod",
             "GL_ARB_shader_objects",
             "GL_ARB_vertex_shader",
             "GL_ARB_fragment_shader",
