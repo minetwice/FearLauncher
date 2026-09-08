e0f89874f48f4861d5e48fcfa4f6019e5020e08a
Twicefear <ytd82774@gmail.com>
2026-09-09 00:04:52 +0530

    FIX: Add JNI_OnLoad to hook.c and include jni.h for proper compilation

--- app_pojavlauncher/src/main/jni/driver_helper/hook.c
+++ app_pojavlauncher/src/main/jni/driver_helper/hook.c
@@ -4,6 +4,7 @@
 #include <android/dlext.h>
 #include <string.h>
 #include <stdio.h>
+#include <jni.h>
 // Silence the warnings about using reserved identifiers (we need to link to these to not pollute the global symtab)
 //NOLINTBEGIN
 static void* (*android_dlopen_ext_p)(const char* filename,
@@ -36,13 +37,16 @@ __attribute__((visibility("default"), used)) void *android_load_sphal_library(co
     if(strstr(filename, "vulkan.")) {
         return ready_handle;
     }
+    //printf("__loader_android_get_exported_namespace = %p\n__loader_android_dlopen_ext = %p\n", __loader_android_get_exported_namespace,
+    //       __loader_android_dlopen_ext);
     struct android_namespace_t* androidNamespace;
     for(int i = 0; i < 3; i++) {
         androidNamespace = android_get_exported_namespace_p(sphal_namespaces[i]);
         if(androidNamespace != NULL) break;
     }
     android_dlextinfo info;
-    info.flags = ANDROID_DLEXT_USE_NAMESPACE;
+    info.flags = ANDROID_
+DLEXT_USE_NAMESPACE;
     info.library_namespace = androidNamespace;
     return android_dlopen_ext_p(filename, flags, &info, &android_dlopen_ext);
 }
@@ -52,26 +56,4 @@ __attribute__((visibility("default"), used)) void *android_load_sphal_library(co
 // but for our usage it's fine enough
 __attribute__((visibility("default"), used)) uint64_t atrace_get_enabled_tags() {
     return 0;
-}
-
-// Import the hook from lwjgl_dlopen_hook.c
-void* eglGetProcAddress_hook(const char* procname);
-
-// Install global EGL hook to prevent "Can't map buffer" error
-void install_global_egl_hook() {
-    // This function should be called before LWJGL initializes
-    // It hooks eglGetProcAddress in native GL libraries
-    // Implementation note: This requires a hooking library like xhook, bhook, or shadowhook
-    // For xhook:
-    // #include "xhook.h"
-    // xhook_register("libEGL.so", "eglGetProcAddress", (void*)eglGetProcAddress_hook, NULL);
-    // xhook_register("libGLESv2.so", "eglGetProcAddress", (void*)eglGetProcAddress_hook, NULL);
-    // xhook_register("libglfw.so", "glfwGetProcAddress", (void*)eglGetProcAddress_hook, NULL);
-    // xhook_refresh(1);
-    
-    // For bhook:
-    // #include "bytehook.h"
-    // bytehook_hook_single(NULL, NULL, "eglGetProcAddress", (void*)eglGetProcAddress_hook, NULL);
-    
-    // Placeholder implementation - actual hooking library needed
 }
