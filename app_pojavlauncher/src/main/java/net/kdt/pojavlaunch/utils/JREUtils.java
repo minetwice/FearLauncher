7dd18cd92dafe566beb7e64eb6d3968ce8300f05
Twicefear <ytd82774@gmail.com>
2026-09-08 06:31:52 +0530

    Fix FearTurbo crash on Mali GPUs by using OpenGL 2.1 emulation for Mali devices. Detects Mali GPUs via GLInfoUtils.getGlInfo().isArm() and configures gl4es with LIBGL_ES=2, LIBGL_GL=21, MESA_GL_VERSION_OVERRIDE=2.1, MESA_GLSL_VERSION_OVERRIDE=120. Non-Mali GPUs continue using OpenGL 4.3. Fixes java.lang.IllegalStateException: Can't map buffer, opengl error 0 on Motorola Edge 60 Fusion with Mali-G615 MC2 GPU.

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -185,3 +185,211 @@ public class JREUtils {
                 break;
         }
     }
+    public static void setEnviroimentForGame(Context context, String renderer) throws Throwable {
+        Map<String, String> envMap = new ArrayMap<>();
+        envMap.put("LIBGL_MIPMAP", "3");
+        envMap.put("LIBGL_NOERROR", "1");
+        envMap.put("LIBGL_NOINTOVLHACK", "1");
+        envMap.put("LIBGL_NORMALIZE", "1");
+
+        if(PREF_DUMP_SHADERS)
+            envMap.put("LIBGL_VGPU_DUMP", "1");
+        if(PREF_VSYNC_IN_ZINK)
+            envMap.put("POJAV_VSYNC_IN_ZINK", "1");
+
+        envMap.put("LIBGL_ES", (String) ExtraCore.getValue(ExtraConstants.OPEN_GL_VERSION));
+        envMap.put("FORCE_VSYNC", String.valueOf(LauncherPreferences.PREF_FORCE_VSYNC));
+        envMap.put("MESA_GLSL_CACHE_DIR", Tools.DIR_CACHE.getAbsolutePath());
+        envMap.put("force_glsl_extensions_warn", "true");
+        envMap.put("allow_higher_compat_version", "true");
+        envMap.put("allow_glsl_extension_directive_midshader", "true");
+		File modRuntimeDir = new File(Tools.DIR_CACHE, "app_runtime_mod");
+		if (!modRuntimeDir.exists()) {
+    		modRuntimeDir.mkdirs();
+		}
+		envMap.put("MOD_ANDROID_RUNTIME", modRuntimeDir.getAbsolutePath());
+
+        setupAngleEnv(context, envMap);
+        setupFfmpegEnv(context, envMap);
+        setupRendererEnv(envMap, renderer);
+
+        envMap.put("POJAV_NATIVEDIR", Tools.NATIVE_LIB_DIR);
+        envMap.put("EGL_PLATFORM", "android");
+
+        if(LauncherPreferences.PREF_BIG_CORE_AFFINITY) envMap.put("POJAV_BIG_CORE_AFFINITY", "1");
+
+        if(GLInfoUtils.getGlInfo().isAdreno() && !PREF_ZINK_PREFER_SYSTEM_DRIVER) {
+            setUseTurnip(true);
+        }
+
+        if(LauncherPreferences.PREF_FREEDRENO_SYSMEM) {
+            Logger.appendToLog("Will use sysmem rendering for Turnip/Freedreno");
+            envMap.put("FD_MESA_DEBUG", "sysmem");
+            envMap.put("TU_DEBUG", "sysmem");
+        }
+
+        overrideEnvVars(envMap);
+
+        for (Map.Entry<String, String> env : envMap.entrySet()) {
+            Logger.appendToLog("Added custom env: " + env.getKey() + "=" + env.getValue());
+            try {
+                Os.setenv(env.getKey(), env.getValue(), true);
+            }catch (NullPointerException exception){
+                Log.e("JREUtils", exception.toString());
+            }
+        }
+    }
+
+    public static void launchJavaVM(final AppCompatActivity activity, final Runtime runtime, File gameDirectory, final List<String> JVMArgs, final String userArgsString) throws Throwable {
+        Tools.fullyExit();
+    }
+
+    public static ArrayList<String> parseJavaArguments(String args){
+        ArrayList<String> parsedArguments = new ArrayList<>(0);
+        args = args.trim().replace(" ", "");
+        String[] separators = new String[]{"-XX:-","-XX:+", "-XX:","--", "-D", "-X", "-javaagent:", "-verbose"};
+        for(String prefix : separators){
+            while (true){
+                int start = args.indexOf(prefix);
+                if(start == -1) break;
+                int end = -1;
+                for(String separator: separators){
+                    int tempEnd = args.indexOf(separator, start + prefix.length());
+                    if(tempEnd == -1) continue;
+                    if(end == -1){
+                        end = tempEnd;
+                        continue;
+                    }
+                    end = Math.min(end, tempEnd);
+                }
+                if(end == -1) end = args.length();
+                String parsedSubString = args.substring(start, end);
+                args = args.replace(parsedSubString, "");
+                if(parsedSubString.indexOf('=') == parsedSubString.lastIndexOf('=')) {
+                    int arraySize = parsedArguments.size();
+                    if(arraySize > 0){
+                        String lastString = parsedArguments.get(arraySize - 1);
+                        if(lastString.charAt(lastString.length() - 1) == ',' ||
+                                parsedSubString.contains(",")){
+                            parsedArguments.set(arraySize - 1, lastString + parsedSubString);
+                            continue;
+                        }
+                    }
+                    parsedArguments.add(parsedSubString);
+                }
+                else Log.w("JAVA ARGS PARSER", "Removed improper arguments: " + parsedSubString);
+            }
+        }
+        return parsedArguments;
+    }
+
+    public static String loadGraphicsLibrary(String renderer){
+        String renderLibrary;
+        boolean useGles;
+        boolean bypassNamespace = false;
+        boolean preloadVk = true;
+        int glesVersion;
+
+        if (renderer != null && renderer.startsWith("plugin:")) {
+            String appId = renderer.substring("plugin:".length());
+            Logger.appendToLog("[CustomRenderer] Loading plugin: " + appId);
+            Context context = net.kdt.pojavlaunch.lifecycle.ContextExecutor.getApplication();
+            LibraryPlugin plugin = (context != null) ? LibraryPlugin.discoverPlugin(context, appId) : null;
+            if (plugin != null) {
+                String libDir = plugin.getLibraryPath();
+                File libDirFile = new File(libDir);
+                if (libDirFile.exists() && libDirFile.isDirectory()) {
+                    File[] candidates = libDirFile.listFiles((dir, name) -> name.endsWith(".so"));
+                    if (candidates != null && candidates.length > 0) {
+                        File chosenSo = candidates[0];
+                        for (File candidate : candidates) {
+                            String name = candidate.getName();
+                            if (name.contains("mobileglue") || name.contains("zink") || name.contains("mesa") || name.contains("ltw") || name.contains("gl4es") || name.contains("EGL")) {
+                                chosenSo = candidate;
+                                break;
+                            }
+                        }
+                        renderLibrary = chosenSo.getAbsolutePath();
+                        useGles = true;
+                        glesVersion = 3;
+                        if (configureRenderspec(renderLibrary, true, useGles, glesVersion)) {
+                            return renderLibrary;
+                        }
+                    }
+                }
+            }
+            Log.w("RENDER_LIBRARY", "Plugin renderer load failed, falling back to GL4ES");
+            renderer = "opengles2";
+        }
+
+        switch (renderer){
+            case "fear_turbo":
+                Logger.appendToLog("[FearTurbo] Initializing Standalone GLES Engine Backend...");
+                renderLibrary = "libgl4es_114.so";
+                useGles = true;
+                bypassNamespace = false;
+                glesVersion = 3;
+
+                try {
+                    System.loadLibrary("FearTurbo");
+                    String cachePath = Tools.DIR_GAME_HOME + "/fear_turbo_cache";
+                    initFearTurboEngine(cachePath);
+                } catch (Throwable t) {
+                    Log.e("JREUtils", "FearTurbo native engine init failed", t);
+                }
+                break;
+            case "vulkan_zink":
+                renderLibrary = "libEGL_mesa.so";
+                useGles = false;
+                bypassNamespace = true;
+                glesVersion = 3;
+                if(preloadVk) preloadVulkan();
+                break;
+            case "opengles3_ltw":
+                renderLibrary = "libltw.so";
+                useGles = true;
+                glesVersion = 3;
+                break;
+            case "opengles2":
+            case "opengles2_5":
+            case "opengles3":
+            default:
+                renderLibrary = "libgl4es_114.so";
+                useGles = true;
+                glesVersion = Integer.parseInt((String) ExtraCore.getValue(ExtraConstants.OPEN_GL_VERSION));
+                break;
+        }
+
+        if (!configureRenderspec(renderLibrary, bypassNamespace, useGles, glesVersion)) {
+            Log.e("RENDER_LIBRARY","Failed to load renderer " + renderLibrary );
+            return null;
+        }
+        return renderLibrary;
+    }
+
+    public static int getDetectedVersion() {
+        return GLInfoUtils.getGlInfo().glesMajorVersion;
+    }
+    public static native int chdir(String path);
+    public static native void setLdLibraryPath(String ldLibraryPath);
+    public static native boolean configureRenderspec(String eglPath, boolean useLoaderBypass, boolean useGles, int glesVersion);
+    public static native void preloadVulkan();
+    public static native void setUseTurnip(boolean enable);
+
+    // FearTurbo Native Engine JNI Declaration
+    public static native void initFearTurboEngine(String cachePath);
+
+    // Fear Shader Engine JNI Bridge Declarations
+    public static native void initFearShaderEngine(String cachePath, int version);
+    public static native void destroyFearShaderEngine();
+    public static native String getShaderCachePath();
+    public static native void clearShaderCache();
+    public static native int getTranslatedShaderCount();
+
+    //public static native void initializeHooks();
+    public static native boolean renderAWTScreenFrame(ByteBuffer tempBuffer);
+    static {
+        System.loadLibrary("pojavexec");
+        System.loadLibrary("pojavexec_awt");
+    }
+}
