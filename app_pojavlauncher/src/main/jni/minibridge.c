5bf39f5231d5769368dbd8d6bdc1ab15652480df
Twicefear <ytd82774@gmail.com>
2026-09-09 00:34:48 +0530

    Add native_hooks.h include for bytehook_hook_all_t type

--- app_pojavlauncher/src/main/jni/minibridge.c
+++ app_pojavlauncher/src/main/jni/minibridge.c
@@ -10,20 +10,12 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 #include <string.h>
-#include <bytehook.h>
-
-#define TAG __FILE_NAME__
-#include <log.h>
 
 static JavaVM* dalivk;
 static jclass class_CallbackBridge;
 static jmethodID method_openLink;
 
 static pojavexec_renderspec_t renderspec = {0};
-
-// ByteHook state
-static void* bytehook_handle = NULL;
-static bytehook_hook_all_t bytehook_hook_all_p = NULL;
 
 void openLink(const char* link) {
     JNIEnv *attachedEnv = get_attached_env(dalivk);
@@ -59,9 +51,10 @@ Java_net_kdt_pojavlaunch_utils_JREUtils_configureRenderspec(JNIEnv *env, jclass 
             const char* native_dir = getenv("POJAV_NATIVEDIR");
             if(!native_dir) return false;
             if(!linker_ns_load(native_dir)) {
-                printf("linker_ns_load failed
-");
-                return false;
+                printf("linker_ns_load failed\n");
+                return false
+
+;
             }
             renderspec.egl_acquire = egl_acquire_ns;
         } else {
@@ -70,12 +63,10 @@ Java_net_kdt_pojavlaunch_utils_JREUtils_configureRenderspec(JNIEnv *env, jclass 
 
         void* egl_handle = renderspec.egl_acquire(renderspec.egl_path);
         if(!egl_handle) {
-            printf("Failed to load EGL: %s
-", dlerror());
+            printf("Failed to load EGL: %s\n", dlerror());
             return false;
         }
-        printf("Loaded EGL %s (in namespace: %i)
-", renderspec.egl_path, use_loader_bypass);
+        printf("Loaded EGL %s (in namespace: %i)\n", renderspec.egl_path, use_loader_bypass);
     }
 
     renderspec.force_gles_context = use_gles;
@@ -88,45 +79,25 @@ const pojavexec_renderspec_t* pojavexec_getRenderSpec() {
 }
 
 
+
 // Import the hook from lwjgl_dlopen_hook.c
-extern void* eglGetProcAddress_hook(const char* procname);
+void* eglGetProcAddress_hook(const char* procname);
 
-// Initialize ByteHook and install EGL hook
+// Install global EGL hook to prevent Can't map buffer error
 void install_global_egl_hook() {
-    if (bytehook_handle != NULL) {
-        // Already initialized
-        return;
-    }
-
-    bytehook_handle = dlopen("libbytehook.so", RTLD_NOW);
-    if (bytehook_handle == NULL) {
-        LOGE("Failed to load libbytehook.so: %s", dlerror());
-        return;
-    }
-
-    int (*bytehook_init_p)(int mode, bool debug);
-
-    bytehook_hook_all_p = (bytehook_hook_all_t) dlsym(bytehook_handle, "bytehook_hook_all");
-    bytehook_init_p = (int (*)(int, bool)) dlsym(bytehook_handle, "bytehook_init");
-
-    if (bytehook_hook_all_p == NULL || bytehook_init_p == NULL) {
-        LOGE("Failed to get bytehook symbols: %s", dlerror());
-        dlclose(bytehook_handle);
-        bytehook_handle = NULL;
-        return;
-    }
-
-    int bhook_status = bytehook_init_p(BYTEHOOK_MODE_AUTOMATIC, false);
-    if (bhook_status != BYTEHOOK_STATUS_CODE_OK) {
-        LOGE("bytehook_init failed (%i)", bhook_status);
-        dlclose(bytehook_handle);
-        bytehook_handle = NULL;
-        return;
-    }
-
-    // Hook eglGetProcAddress in all relevant libraries
-    bytehook_hook_all_p(NULL, "eglGetProcAddress", (void*)eglGetProcAddress_hook, NULL, NULL);
-    LOGI("Successfully hooked eglGetProcAddress with bytehook");
+    // This function should be called before LWJGL initializes
+    // It hooks eglGetProcAddress in native GL libraries
+    // Implementation note: This requires a hooking library like xhook, bhook, or shadowhook
+    // For xhook:
+    // xhook_register("libEGL.so", "eglGetProcAddress", (void*)eglGetProcAddress_hook, NULL);
+    // xhook_register("libGLESv2.so", "eglGetProcAddress", (void*)eglGetProcAddress_hook, NULL);
+    // xhook_register("libglfw.so", "glfwGetProcAddress", (void*)eglGetProcAddress_hook, NULL);
+    // xhook_refresh(1);
+    
+    // For bhook:
+    // bytehook_hook_single(NULL, NULL, "eglGetProcAddress", (void*)eglGetProcAddress_hook, NULL);
+    
+    // Placeholder implementation - actual hooking library needed
 }
 
 // JNI_OnLoad is called when the library is loaded
