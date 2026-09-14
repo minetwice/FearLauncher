64198d1ade1b3b83a2655c977f880f0c3158459e
Twicefear <ytd82774@gmail.com>
2026-09-15 01:36:45 +0530

    Rewrite PojavApplication.java with correct string literals and TurnipZinkManager initialization - FINAL FIX

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavApplication.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavApplication.java
@@ -42,7 +42,6 @@ public class PojavApplication extends Application {
 					ActivityCompat.checkSelfPermission(PojavApplication.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) && Tools.checkStorageRoot(PojavApplication.this);
 			File crashFile = new File(storagePermAllowed ? Tools.DIR_GAME_HOME : Tools.DIR_DATA, "latestcrash.txt");
 			try {
-				// Write to file, since some devices may not able to show error
 				FileUtils.ensureParentDirectory(crashFile);
 				PrintStream crashStream = new PrintStream(crashFile);
 				crashStream.append("PojavLauncher crash report
@@ -73,29 +72,19 @@ public class PojavApplication extends Application {
 	public void onCreate() {
 		ContextExecutor.setApplication(this);
 		TurnipZinkManager.initialize();
-		// Disable fatal errors on gplay. This is necessary so that google can collect crash report data and send it to me
-		// (where i can find the cause and fix it)
-        //noinspection ConstantValue
-        if(!BuildConfig.BUILD_TYPE.equals("gplay")) installFatalErrorHandler();
+		if(!BuildConfig.BUILD_TYPE.equals("gplay")) installFatalErrorHandler();
 		
 		try {
 			super.onCreate();
 			if(Tools.checkStorageRoot(this)){
-				// Implicitly initializes early constants and storage constants.
-				// Required to run the main activity properly.
 				LauncherPreferences.loadPreferences(this);
 			} else {
-				// In other cases, only initialize enough for the basicmost basics to work
-				// and not explode.
 				Tools.initEarlyConstants(this);
 			}
 			Tools.DEVICE_ARCHITECTURE = Architecture.getDeviceArchitecture();
-			//Force x86 lib directory for Asus x86 based zenfones
 			if(Architecture.isx86Device() && Architecture.is32BitsDevice()){
 				String originalJNIDirectory = getApplicationInfo().nativeLibraryDir;
-				getApplicationInfo().nativeLibraryDir = originalJNIDirectory.substring(0,
-												originalJNIDirectory.lastIndexOf("/"))
-												.concat("/x86");
+				getApplicationInfo().nativeLibraryDir = originalJNIDirectory.substring(0, originalJNIDirectory.lastIndexOf("/")).concat("/x86");
 			}
             MinecraftDownloader.prepareSubstitutionMap(getAssets());
 			AsyncAssetManager.unpackRuntime(getAssets());
