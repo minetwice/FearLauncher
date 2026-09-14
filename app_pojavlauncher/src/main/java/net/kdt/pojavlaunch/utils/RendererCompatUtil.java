1d2d366dc1e89ec87d9bc1590571c8638552352f
Twicefear <ytd82774@gmail.com>
2026-09-14 06:50:44 +0530

    panvk_zink: UI list + RendererCompat always show PanVK Zink

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/RendererCompatUtil.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/RendererCompatUtil.java
@@ -1,49 +1,35 @@
 package net.kdt.pojavlaunch.utils;
 
-import static android.os.Build.VERSION.SDK_INT;
-
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.content.res.Resources;
-import android.os.Build;
 
-import net.kdt.pojavlaunch.Architecture;
+import net.kdt.pojavlaunch.R;
 import net.kdt.pojavlaunch.Tools;
+import net.kdt.pojavlaunch.prefs.LauncherPreferences;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
-import git.artdeell.mojo.R;
 
 public class RendererCompatUtil {
     private static RenderersList sCompatibleRenderers;
 
-    public static boolean checkVulkanSupport(PackageManager packageManager) {
-        if(SDK_INT >= Build.VERSION_CODES.N) {
-            return packageManager.hasSystemFeature(PackageManager.FEATURE_VULKAN_HARDWARE_LEVEL) &&
-                    packageManager.hasSystemFeature(PackageManager.FEATURE_VULKAN_HARDWARE_VERSION);
-        }
-        return false;
-    }
-
-    /** Return the renderers that are compatible with this device */
     public static RenderersList getCompatibleRenderers(Context context) {
         if(sCompatibleRenderers != null) return sCompatibleRenderers;
+
         Resources resources = context.getResources();
         String[] defaultRenderers = resources.getStringArray(R.array.renderer_values);
         String[] defaultRendererNames = resources.getStringArray(R.array.renderer);
-        boolean deviceHasVulkan = checkVulkanSupport(context.getPackageManager());
-        // Current Mesa requires API29+
-        boolean deviceCompatibleMesa = SDK_INT >= 29;
-        boolean deviceHasOpenGLES3 = JREUtils.getDetectedVersion() >= 3;
-        // LTW is an optional dependency
-        boolean appHasLtw = new File(Tools.NATIVE_LIB_DIR, "libltw.so").exists();
+
+        boolean deviceHasVulkan = Tools.checkVulkanSupport(context.getPackageManager());
+        boolean deviceCompatibleMesa = Tools.checkDeviceCompatibleMesa();
+        boolean deviceHasOpenGLES3 = Tools.checkOpenGLES3Support();
+        boolean appHasLtw = Tools.checkLocalLibraryPresent("libltw.so");
+
         List<String> rendererIds = new ArrayList<>(defaultRenderers.length);
         List<String> rendererNames = new ArrayList<>(defaultRendererNames.length);
         for(int i = 0; i < defaultRenderers.length; i++) {
             String rendererId = defaultRenderers[i];
-            if(rendererId.equals("turnip_zink")) {
+            if(rendererId.equals("turnip_zink") || rendererId.equals("panvk_zink")) {
                 rendererIds.add(rendererId);
                 rendererNames.add(defaultRendererNames[i]);
                 continue;
