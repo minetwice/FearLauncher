e49b2ebd8187303b33d8428923236cf597e66bb7
Twicefear <ytd82774@gmail.com>
2026-09-15 22:48:20 +0530

    Fix PojavApplication.java: Correct string literals and add TurnipZinkManager initialization for Mali-G615 GPU

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavApplication.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavApplication.java
@@ -1,3 +1,109 @@
 package net.kdt.pojavlaunch;
 
-public class PojavApplication extends Application {}
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import net.kdt.pojavlaunch.lifecycle.ContextExecutor;
+import net.kdt.pojavlaunch.gpu.TurnipZinkManager;
+import net.kdt.pojavlaunch.prefs.LauncherPreferences;
+import net.kdt.pojavlaunch.tasks.AsyncAssetManager;
+import net.kdt.pojavlaunch.tasks.MinecraftDownloader;
+import net.kdt.pojavlaunch.utils.FileUtils;
+import net.kdt.pojavlaunch.utils.LocaleUtils;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import git.artdeell.mojo.BuildConfig;
+
+public class PojavApplication extends Application {
+	public static final String CRASH_REPORT_TAG = "PojavCrashReport";
+	public static final ExecutorService sExecutorService = new ThreadPoolExecutor(4, 4, 500, TimeUnit.MILLISECONDS,  new LinkedBlockingQueue<>());
+
+	private void installFatalErrorHandler() {
+		Thread.setDefaultUncaughtExceptionHandler((thread, th) -> {
+			boolean storagePermAllowed = (Build.VERSION.SDK_INT < 23 || Build.VERSION.SDK_INT >= 29 ||
+					ActivityCompat.checkSelfPermission(PojavApplication.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) && Tools.checkStorageRoot(PojavApplication.this);
+			File crashFile = new File(storagePermAllowed ? Tools.DIR_GAME_HOME : Tools.DIR_DATA, "latestcrash.txt");
+			try {
+				FileUtils.ensureParentDirectory(crashFile);
+				PrintStream crashStream = new PrintStream(crashFile);
+				crashStream.append("PojavLauncher crash report\n");
+				crashStream.append(" - Time: ").append(DateFormat.getDateTimeInstance().format(new Date())).append("\n");
+				crashStream.append(" - Device: ").append(Build.PRODUCT).append(" ").append(Build.MODEL).append("\n");
+				crashStream.append(" - Android version: ").append(Build.VERSION.RELEASE).append("\n");
+				crashStream.append(" - Crash stack trace:\n");
+				crashStream.append(" - Launcher version: " + BuildConfig.VERSION_NAME + "\n");
+				crashStream.append(Log.getStackTraceString(th));
+				crashStream.close();
+			} catch (Throwable throwable) {
+				Log.e(CRASH_REPORT_TAG, " - Exception attempt saving crash stack trace:", throwable);
+				Log.e(CRASH_REPORT_TAG, " - The crash stack trace was:", th);
+			}
+
+			FatalErrorActivity.showError(PojavApplication.this, crashFile.getAbsolutePath(), storagePermAllowed, th);
+			Tools.fullyExit();
+		});
+	}
+
+	@Override
+	public void onCreate() {
+		ContextExecutor.setApplication(this);
+		TurnipZinkManager.initialize();
+		if(!BuildConfig.BUILD_TYPE.equals("gplay")) installFatalErrorHandler();
+		
+		try {
+			super.onCreate();
+			if(Tools.checkStorageRoot(this)){
+				LauncherPreferences.loadPreferences(this);
+			} else {
+				Tools.initEarlyConstants(this);
+			}
+			Tools.DEVICE_ARCHITECTURE = Architecture.getDeviceArchitecture();
+			if(Architecture.isx86Device() && Architecture.is32BitsDevice()){
+				String originalJNIDirectory = getApplicationInfo().nativeLibraryDir;
+				getApplicationInfo().nativeLibraryDir = originalJNIDirectory.substring(0, originalJNIDirectory.lastIndexOf("/")).concat("/x86");
+			}
+			MinecraftDownloader.prepareSubstitutionMap(getAssets());
+			AsyncAssetManager.unpackRuntime(getAssets());
+		} catch (Throwable throwable) {
+			Intent ferrorIntent = new Intent(this, FatalErrorActivity.class);
+			ferrorIntent.putExtra("throwable", throwable);
+			ferrorIntent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+			startActivity(ferrorIntent);
+		}
+	}
+
+	@Override
+	public void onTerminate() {
+		super.onTerminate();
+		ContextExecutor.clearApplication();
+	}
+
+	@Override
+	protected void attachBaseContext(Context base) {
+		super.attachBaseContext(LocaleUtils.setLocale(base));
+	}
+
+	@Override
+	public void onConfigurationChanged(@NonNull Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		LocaleUtils.setLocale(this);
+	}
+}
