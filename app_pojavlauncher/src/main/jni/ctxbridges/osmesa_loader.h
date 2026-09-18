6ab1db9741e1a164dbd69f7b47ed8b1671fa0bd2
Twicefear <ytd82774@gmail.com>
2026-09-18 18:59:27 +0530

    osmesa_loader: export OSMesaGetProcAddress_p

--- app_pojavlauncher/src/main/jni/ctxbridges/osmesa_loader.h
+++ app_pojavlauncher/src/main/jni/ctxbridges/osmesa_loader.h
@@ -21,6 +21,7 @@ extern void (*glClearColor_p) (GLclampf red, GLclampf green, GLclampf blue, GLcl
 extern void (*glClear_p) (GLbitfield mask);
 extern void (*glReadPixels_p) (GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, void * data);
 extern void (*glReadBuffer_p) (GLenum mode);
+extern void* (*OSMesaGetProcAddress_p) (const char* funcName);
 
 void dlsym_OSMesa();
 bool osmesa_is_loaded();
