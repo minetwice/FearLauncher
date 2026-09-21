0fec9a3c6798a6ca242cb6d3b3a3ca6df4e32de1
Twicefear <ytd82774@gmail.com>
2026-09-21 18:47:38 +0530

    chore: cleanup MC6 workflow (all build fixes complete)

--- scripts/mc6.py
+++
@@ -1,9 +0,0 @@
-p = 'app_pojavlauncher/src/main/res/values/strings.xml'
-t = open(p, encoding='utf-8').read()
-old = 'You don' + chr(39) + 't seem to have an Xbox Live account.'
-new = 'You don' + chr(92) + chr(39) + 't seem to have an Xbox Live account.'
-assert t.count(old) == 1
-open(p, 'w', encoding='utf-8', newline='').write(t.replace(old, new))
-import xml.etree.ElementTree as ET
-ET.parse(p)
-print('MC6 OK')
