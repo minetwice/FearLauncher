420e78e254bf4932eeed23fcbf1e65aee05bb919
Twicefear <ytd82774@gmail.com>
2026-09-23 02:50:23 +0530

    MC20 diag parts 2-4/10 (cast-free, re-push)

--- scripts/diag_parts/part_04.py
+++ scripts/diag_parts/part_04.py
@@ -1,2 +1,2 @@
 OLD = b'    __android_log_print(ANDROID_LOG_WARN, g_LogTag, "No native surface \xe2\x80\x94 color buffer only");'
-NEW = b'    __android_log_print(ANDROID_LOG_WARN, g_LogTag, "No native surface \xe2\x80\x94 color buffer only");\n    fprintf(stderr, "OSMDIAG: no native surface (pojavWindow=%p) \xe2\x80\x94 rendering disabled\\n",\n            (void*X©ridge_environ.pojavWindow);'
+NEW = b'    __android_log_print(ANDROID_LOG_WARN, g_LogTag, "No native surface \xe2\x80\x94 color buffer only");\n    fprintf(stderr, "OSMDIAG: no native surface (pojavWindow=%p) \xe2\x80\x94 rendering disabled\\n",\n            bridge_environ.pojavWindow);'
